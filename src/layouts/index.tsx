36ecd806a4714f02f3bf285f50e8268b7ec27204
Maulana Sodiqin <maulanasdqn@gmail.com>
2022-11-26 03:08:51 +0700

    Maulana Sodiqin | Add Husky

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,7 @@
 import { FC, ReactElement } from "react";
 
 const HomePageLayouts: FC = (): ReactElement => {
-  return (
-    <section className="flex items-center justify-center w-auto h-auto"></section>
-  );
+  return <section className="flex items-center justify-center w-auto h-auto"></section>;
 };
 
 export default HomePageLayouts;
