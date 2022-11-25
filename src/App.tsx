3cd4b3d8215457392539952cc4932ea8c5009e6d
Maulana Sodiqin <maulanasdqn@gmail.com>
2022-11-26 03:28:58 +0700

    Maulana Sodiqin | Fix Navbar Floating

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { FC, ReactElement } from "react";
+import { FC, Fragment, ReactElement } from "react";
 import ReactLogo from "./assets/react.svg";
 import Navbar from "./components/Navbar";
 const App: FC = (): ReactElement => {
   return (
-    <section className="items-center flex justify-center w-full h-screen flex-col">
+    <Fragment>
       <Navbar />
-      <img data-testid="React Logo" width={400} src={ReactLogo} alt="react-logo" />
-      <h1 className="text-black-900 text-2xl font-sans">Hello there</h1>
-      <span className="font-sans">this is my personal web</span>
-      <h1 className="text-black-900 text-4xl font-sans">Maulana Sodiqin</h1>
-    </section>
+      <section className="items-center flex justify-center w-full h-screen flex-col">
+        <img data-testid="React Logo" width={400} src={ReactLogo} alt="react-logo" />
+        <h1 className="text-black-900 text-2xl font-sans">Hello there</h1>
+        <span className="font-sans">this is my personal web</span>
+        <h1 className="text-black-900 text-4xl font-sans">Maulana Sodiqin</h1>
+      </section>
+    </Fragment>
   );
 };
 
