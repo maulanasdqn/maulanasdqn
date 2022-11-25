aea4662fc866e3944cca935bf4773ee5c561b5e7
Maulana Sodiqin <maulanasdqn@gmail.com>
2022-11-26 02:53:54 +0700

    Maulana Sodiqin | Improvement Unit Testing

--- src/components/Common/TextField.tsx
+++ src/components/Common/TextField.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactElement } from "react";
 
 const TextField: FC = (): ReactElement => {
-  return <input className="w-auto h-auto" />;
+  return <input data-testid="input" className="w-auto h-auto" />;
 };
 
 export default TextField;
